11ebe50beae19833450609ccab0858baaaf50244
farisubuntu <farisubuntu@gmail.com>
2024-03-08 09:19:22 +0300

    fix english

--- categories.js
+++ categories.js
@@ -26,8 +26,6 @@ const categories = [
   "29",
   "30",
   "31",
-  "32",
-  "33",
   "34",
   "35",
   "36",
@@ -41,4 +39,4 @@ const categories = [
   "91",
 ];
 
-export { categories };
+export default categories;
--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,12 @@
 import fs from "fs";
-import { categories } from "./categories.js";
+import categories from "./categories.js";
 
 let AudioArray = [];
 let ImageArray = [];
 let target_lang = "en_gb"; // change this per target language
 // function to extract image and audio links from quizzes
 // TODO: dont forget vocabularies.json
+
 function extractUrls(quiz) {
   let quiz_type = quiz.type;
   switch (quiz_type) {
@@ -33,6 +34,8 @@ function extractUrls(quiz) {
 
     case "T1b":
     case "T1":
+    case "T2":
+    case "T2b":
     case "R":
     case "C1b":
     case "C2b":
@@ -54,13 +57,13 @@ function extractUrls(quiz) {
       break;
 
     default:
-      console.log("quiz type: ", quiz_type, " ignored.........");
+      console.log(`${quiz_type} ignored in lesson id: ${quiz.lesson}`);
   }
 }
 
 function getCategory(category_id) {
   for (let i = 1; i <= 6; i++) {
-    let rawdata = fs.readFileSync(`./${category_id}/${category_id}0${i}.json`);
+    let rawdata = fs.readFileSync(`./data/${category_id}/${category_id}0${i}.json`);
     let topic_data = JSON.parse(rawdata);
     let quizzes = topic_data.quizzes;
     quizzes.forEach((quiz) => {
@@ -69,18 +72,14 @@ function getCategory(category_id) {
   }
 }
 
-// getUrls
-
-categories.forEach((category_id) => {
-  getCategory(category_id);
-  setTimeout(function () {
-    console.log("category_id: ", category_id, " done.........");
-  }, 2000);
-});
+// getCategory("1");
 
 // remove duplicate array using `Sets` which allows you to create collections of unique values:
 // for other ways see: https://builtin.com/software-engineering-perspectives/remove-duplicates-from-array-javascript
-
+categories.forEach((x) => {
+  console.log(x);
+  getCategory(x);
+});
 function removeDubplicates(arr) {
   return [...new Set(arr)];
 }
@@ -91,20 +90,25 @@ let images_set = removeDubplicates(ImageArray);
 // console.log('joined audios_set:');
 // console.log(audios_set.join('\n')); // working
 
-// wait 2 seconds before writing audio array
-setTimeout(function () {
-  console.log(">>>  writing audio array to audio_urls.txt....");
-}, 2000);
-fs.appendFileSync("audios/audios_urls.txt", audios_set.join("\n"), (err) => {
+// log urls
+console.log('audios_set: ');
+console.log(audios_set);
+console.log("#########################");
+console.log('images_set');
+console.log(images_set);
+
+// console.log("Audio Urls: ")
+// console.log(AudioArray);
+// console.log("#########################");
+// console.log("Image Urls:")
+// console.log(ImageArray);
+// console.log("writing audio array to audio_urls.txt....");
+
+fs.appendFileSync("./audios_urls.txt", audios_set.join("\n"), (err) => {
   if (err) console.log(err);
   console.log("OK...");
 });
-// wait 2 seconds before writing image array
-setTimeout(function () {
-  console.log(">>>  writing image array to images_urls.txt....");
-}, 2000);
-
-fs.appendFileSync("images/images_urls.txt", images_set.join("\n"), (err) => {
+fs.appendFileSync("./images_urls.txt", images_set.join("\n"), (err) => {
   if (err) console.log(err);
   console.log("OK...");
 });
